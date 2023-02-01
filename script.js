8ae5366f34167d0e8753c85ac652ee97e6a4210e
ganiiking <101539117+ganiiking@users.noreply.github.com>
2023-02-01 12:10:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnE = document.getElementById("btn");
+
+
+
+function myFunction(){
+    document.getElementById('img').style.display ="block"
+    document.getElementById('img').src='https://source.unsplash.com/random/?Cutecat/>';
+}
+
+function refreshPage(){
+    window.location.reload();
+}
