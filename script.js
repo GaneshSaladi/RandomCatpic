626190463533828da0770e6d70cd6d7ee6330a03
ganiiking <101539117+ganiiking@users.noreply.github.com>
2023-02-02 06:41:24 +0530

    updating functions for dark-mode

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const btnE = document.getElementById("btn");
 
+// Function for getting random images
 
-
-function myFunction(){
-    document.getElementById('img').style.display ="block"
-    document.getElementById('img').src='https://source.unsplash.com/random/?Cutecat/>';
+function myFunction() {
+    document.getElementById('img').style.display = "block"
+    document.getElementById('img').src = 'https://source.unsplash.com/random/?Cutecat/>';
 }
 
-function refreshPage(){
+function refreshPage() {
     window.location.reload();
+}
+
+//function for darkmode
+
+function darkmode() {
+    document.getElementById('dark').className = "darkmode";
+    document.getElementById('container').className = "containerdark";
 }
